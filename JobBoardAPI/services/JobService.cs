69ff6c1be88bc742a63a8e0b43e2d00a9d8ce0ec
Hossein Jami <hjami@synergetic.net.au>
2019-10-04 16:29:45 +1000

    Add changes

--- JobBoardAPI/services/JobService.cs
+++ JobBoardAPI/services/JobService.cs
@@ -7,15 +7,15 @@ using Newtonsoft.Json;
 
 namespace JB.services
 {
-    
+
     public interface IJobService
     {
-      List<Job> GetCSVPayload(string csvJobs);
+        List<Job> GetCSVPayload(string csvJobs);
     }
-    
-    public class JobService: IJobService
+
+    public class JobService : IJobService
     {
-        
+
         public JobBoardStoreService JobBoardStoreService { get; set; }
         public JobService(JobBoardStoreService _jobBoardStoreService)
         {
@@ -25,72 +25,81 @@ namespace JB.services
         public List<Job> GetCSVPayload(string csvJobs)
         {
             var lines = csvJobs.TrimEnd().Split('\n').ToList();
-            var titles = new List<string> {"Title", "Description", "PostingDate", "Location", "Applicants"}; //lines[0].Split(',').ToList();
+            var titles = new List<string> { "Title", "Description", "PostingDate", "Location", "Applicants" }; //lines[0].Split(',').ToList();
             var contents = lines.
                             Skip(1).
                             Select((s) => s.TrimEnd()).ToList();
-                            
 
-            var json = 
+
+            var json =
                 contents.
-                Select((line) => { 
+                Select((line) =>
+                {
                     if (line.EndsWith('"'))
                     {
-                    var indx = line.Substring(0, line.Length - 1).LastIndexOf('"');
-                    var lastCol = line.Substring(indx + 1, line.Length - indx - 2).Replace(',', ';');;
-                    line = line.Remove(indx) + lastCol;
+                        var indx = line.Substring(0, line.Length - 1).LastIndexOf('"');
+                        var lastCol = line.Substring(indx + 1, line.Length - indx - 2).Replace(',', ';'); ;
+                        line = line.Remove(indx) + lastCol;
                     }
                     return line;
-                }).        
+                }).
                 Select(
-                (content) => {
-                    var result = 
+                (content) =>
+                {
+                    var result =
                         content.Split(',').
-                        Select((item, i) => {
+                        Select((item, i) =>
+                        {
                             var res = "";
                             if (i < titles.Count - 1)
-                                {
+                            {
                                 if (titles[i] == "PostingDate")
                                 {
-                                    item = DateTime.Parse(item).ToString("yyyy-MM-dd");
+                                    //item = DateTime.Parse(item).ToString("yyyy-MM-dd");
+                                    item = new DateTime(int.Parse(item.Substring(6, 4)),
+                                                        int.Parse(item.Substring(3, 2)),
+                                                        int.Parse(item.Substring(0, 2)))
+                                              .ToString("yyyy-MM-dd");
                                 }
                                 res = $"\"{titles[i]}\":\"{item}\"";
-                                }
-                                else
+                            }
+                            else
+                            {
+                                if (item.StartsWith('"'))
                                 {
-                                if (item.StartsWith('"')) {
                                     item = item.Remove(0);
                                 };
-                                if (item.EndsWith('"')) {
+                                if (item.EndsWith('"'))
+                                {
                                     item = item.Remove(item.Length - 1);
                                 }
-                                res = 
+                                res =
                                     item.Split(';').
-                                        Distinct() .
-                                        Select((name) => $"{{\"name\":\"{name}\"}}" ).
-                                        Aggregate("_", (acc,_item) => acc == "_" ? _item : acc + "," + _item);
+                                        Distinct().
+                                        Select((name) => $"{{\"name\":\"{name}\"}}").
+                                        Aggregate("_", (acc, _item) => acc == "_" ? _item : acc + "," + _item);
                                 res = $"\"{titles[i]}\":[{res}]";
-                                }
-                                return res;
-                            }).
-                        Aggregate("_", (acc,_item) => (acc == "_") ? _item : acc + "," + _item);
-                        return $"{{{result}}}";    
-                    }
-                ).
-                Aggregate("_", (acc,_item) => (acc == "_") ? _item : acc + "," + _item);
-                
-                json = $"[{json}]";
-
-                //json = "[{\"Title\":\"Ahmad\", \"PostingDate\":\"2019-09-29\", \"Applicants\": [{\"Name\":\"Ali\"}]}]";
-                
-                JsonSerializer js = new JsonSerializer();
-                var objs = js.Deserialize<List<Job>>(new JsonTextReader(new StringReader(json)));
-
-                foreach (var obj in objs)
-                {
-                    JobBoardStoreService.Create(obj);
+                            }
+                            return res;
+                        }).
+                        Aggregate("_", (acc, _item) => (acc == "_") ? _item : acc + "," + _item);
+                    return $"{{{result}}}";
                 }
-                return objs;
+                ).
+                Aggregate("_", (acc, _item) => (acc == "_") ? _item : acc + "," + _item);
+
+            json = $"[{json}]";
+
+            //json = "[{\"Title\":\"Ahmad\", \"PostingDate\":\"2019-09-29\", \"Applicants\": [{\"Name\":\"Ali\"}]}]";
+
+            JsonSerializer js = new JsonSerializer();
+            var objs = js.Deserialize<List<Job>>(new JsonTextReader(new StringReader(json)));
+
+            foreach (var obj in objs)
+            {
+                JobBoardStoreService.Create(obj);
+            }
+            return objs;
 
         }
     }
